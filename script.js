f122eb1aefab6f5c0f0ea72f27c510575a541618
Niloufar <nilooooofar97@gmail.com>
2023-10-26 21:56:37 +0200

    delete hamburger menu

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenue(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
